ebf2de5a32b906f61f14f290fe3ba0f879aaf3bf
Shaun S <shaun.sticka@outlook.com>
2021-12-23 20:50:13 -0600

    updated tables for cross references

--- db/schema.sql
+++ db/schema.sql
@@ -3,18 +3,25 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
 
 CREATE TABLE departments (
-id INTEGER AUTO_INCREMENT PRIMARY KEY,
+id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
 department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
-salary DECIMAL (6,0)
+salary DECIMAL (6,0),
+department_id INTEGER NOT NULL,
+CONSTRAINT fk_departments FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
+-- include employee table with additional job title and salary information in case they differ from the standard set in roles
 CREATE TABLE employees (
 id INTEGER AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL
+last_name VARCHAR(30) NOT NULL,
+job_title VARCHAR(50),
+role_id INTEGER NOT NULL,
+manager_id INTEGER DEFAULT NULL,
+CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id)
 );
